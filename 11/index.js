dfe4a8437c22081b986a23d76b1fd3753affb421
Abylay <Neveryun1212@gmail.com>
2024-01-04 22:50:14 -0800

    FEAT: my first program in JS, Count MOVIES in prompt

--- 11/index.js
+++ 11/index.js
@@ -1,9 +1,26 @@
 "use strict";
+/*
+ let num = 2;
 
-// let num = 2;
-
-// console.log(4/0)
+console.log(4/0)
 
 const clases = `2+2 = 3 ${2 + 2 === 3 ? "da" : "net"}`;
 
 console.log(clases);
+*/
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const lastSeeFilm = prompt("Один из последних просмотренных фильмов?", "");
+const ratingFilm = +prompt("Ваша оценка фильма по 10 шкале: ", "");
+
+personalMovieDB.movies[lastSeeFilm] = ratingFilm;
+console.log(personalMovieDB);
